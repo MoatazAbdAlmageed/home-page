4f413828656944046d6b3eec3ecb83024a80e388
Moataz Mohammady <moataz@wuilt.com>
2020-12-23 20:39:27 +0200

    add urls

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -13,11 +13,24 @@ function Repos() {
                 <div class="card">
                   <div class="card__content">
                     <div class="card__title">{project.name}</div>
+                    {project.alpha && (
+                      <a target="_blank" href={`${project.alpha}`}>
+                        alpha
+                      </a>
+                    )}{" "}
+                    {project.production && (
+                      <a target="_blank" href={`${project.production}`}>
+                        production
+                      </a>
+                    )}
                     <p class="card__text">
                       {project.repos.map((repo) => {
                         return (
                           <>
                             <h2>{repo.name}</h2>
+                            <a target="_blank" href={`${repo.url}/commits/`}>
+                              commits
+                            </a>{" "}
                             <a target="_blank" href={`${repo.url}/issues`}>
                               issues
                             </a>{" "}
@@ -26,9 +39,6 @@ function Repos() {
                             </a>{" "}
                             <a target="_blank" href={`${repo.url}/pulls/`}>
                               pulls
-                            </a>{" "}
-                            <a target="_blank" href={`${repo.url}/commits/`}>
-                              commits
                             </a>{" "}
                             <a target="_blank" href={`${repo.url}/wiki/`}>
                               wiki
