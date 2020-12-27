59c40a7a2093cdc546176dcb9b482b1972c54142
Moataz Mohammady <moataz@wuilt.com>
2020-12-27 19:18:38 +0200

    wip

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <>
       <Container>
+        <a target="_blank" href="https://wakatime.com/dashboard">
+          always use wakatime
+        </a>
         <Repos />
         <Stack />
         <VSCode />
